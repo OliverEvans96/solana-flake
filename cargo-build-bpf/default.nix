55b5e3343f9a078bdcdd7426a2f6fdd572246a0b
Oliver Evans <oliverevans96@gmail.com>
2025-01-22 11:50:30 -0800

    Use import instead of subflakes

---
+++ cargo-build-bpf/default.nix
@@ -0,0 +1,37 @@
+inputs:
+with import inputs.nixpkgs { system = "x86_64-linux"; };
+rustPlatform.buildRustPackage rec {
+  pname = "solana-cargo-build-bpf";
+  version = "1.10.29";
+
+  src = fetchFromGitHub {
+    owner = "solana-labs";
+    repo = "solana";
+    rev = "v${version}";
+    hash = "sha256-tw6WLSZXnqTVr7avQSJf4N4Fz87VTjbQtA5PXjiheUY=";
+  };
+  buildAndTestSubdir = "sdk/cargo-build-bpf";
+
+  cargoHash = "sha256-b+bEbq2F+a8XkvNw9rqQJeQzynvj8zvAUvKJwi2tRs0=";
+
+  nativeBuildInputs = [ pkg-config perl cmake clang libclang.lib ];
+
+  buildInputs = [ udev clang libclang.lib ];
+
+  LIBCLANG_PATH = "${libclang.lib}/lib";
+  NIX_CFLAGS_COMPILE = "-I${libclang.lib}/clang/11.1.0/include";
+
+  doCheck = false;
+
+  cargoPatches = [
+    ./patches/main.rs.diff
+    ./patches/Cargo.toml.diff
+    ./patches/Cargo.lock.diff
+  ];
+
+  meta = with lib; {
+    homepage =
+      "https://github.com/solana-labs/solana/tree/master/sdk/cargo-build-bpf";
+    platforms = platforms.linux;
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,45 +1,39 @@
 {
   description = "Solana";
-  inputs = {
-    solana-bpf-tools.url = "path:./solana-bpf-tools";
-    solana-cli.url = "path:./solana-cli";
-    cargo-build-bpf.url = "path:./cargo-build-bpf";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  outputs = inputs: with import inputs.nixpkgs { 
-      system = "x86_64-linux";
-  }; rec {
-    packages.x86_64-linux.default = with import inputs.nixpkgs { 
-      system = "x86_64-linux";
-    }; stdenv.mkDerivation rec {
-      name = "solana-${version}";
-      version = "1.23.1";
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  outputs = inputs:
+    with import inputs.nixpkgs { system = "x86_64-linux"; }; rec {
+      packages.x86_64-linux.default =
+        with import inputs.nixpkgs { system = "x86_64-linux"; };
+        let solana-cli = (import ./solana-cli/default.nix) inputs;
+        solana-bpf-tools = (import ./solana-bpf-tools/default.nix) inputs;
+        cargo-build-bpf = (import ./cargo-build-bpf/default.nix) inputs;
+        in stdenv.mkDerivation rec {
+          name = "solana-${version}";
+          version = "1.23.1";
 
-      phases = [ "installPhase" ];
+          phases = [ "installPhase" ];
 
-      installPhase = ''
-        mkdir -p $out
-        cp -rf ${inputs.solana-cli.packages.x86_64-linux.default}/* $out
-        chmod 0755 -R $out;
+          installPhase = ''
+            mkdir -p $out
+            cp -rf ${solana-cli}/* $out
+            chmod 0755 -R $out;
 
-        cp -rf ${inputs.cargo-build-bpf.packages.x86_64-linux.default}/* $out
-        chmod 0755 -R $out;
+            cp -rf ${cargo-build-bpf}/* $out
+            chmod 0755 -R $out;
 
-        mkdir -p $out/bin/sdk/bpf
-        cp -rf ${inputs.solana-bpf-tools.packages.x86_64-linux.default}/* $out/bin/sdk/bpf/
-        chmod 0755 -R $out;
-      '';
+            mkdir -p $out/bin/sdk/bpf
+            cp -rf ${solana-bpf-tools}/* $out/bin/sdk/bpf/
+            chmod 0755 -R $out;
+          '';
 
-      meta = with lib; {
-        homepage = "https://github.com/solana-labs";
-        platforms = platforms.linux;
-      };
+          meta = with lib; {
+            homepage = "https://github.com/solana-labs";
+            platforms = platforms.linux;
+          };
+        };
+
+      devShells.x86_64-linux.default =
+        mkShellNoCC { packages = with packages.x86_64-linux; [ default ]; };
     };
-
-    devShells.x86_64-linux.default =  mkShellNoCC {
-      packages = with packages.x86_64-linux; [
-        default
-      ];
-    };
-  };
 }
